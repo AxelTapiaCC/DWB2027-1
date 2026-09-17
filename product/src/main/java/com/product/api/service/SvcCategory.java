4aa5929ca404d786e37f97ec232263daac1809a4
Alan Vladimir Alonso Sanchez <alan_vladimir@ciencias.unam.mx>
2026-09-16 21:07:23 -0600

    Implementacion practica 3, correccion de carpetas y archivos

---
+++ product/src/main/java/com/product/api/service/SvcCategory.java
@@ -0,0 +1,11 @@
+package com.product.api.service;
+import com.product.api.entity.Category;
+
+import java.util.List;
+
+
+
+public interface SvcCategory {
+
+    public List<Category> getCategories();
+}
